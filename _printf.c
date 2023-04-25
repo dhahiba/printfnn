497b1a5ce5bab4b12a6c8ce987a5de9659cd26f5
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-25 14:36:24 -0700

    just try something

--- _printf.c
+++ _printf.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
+
+
 /**
  * _printf - ...
  * @format: ...
@@ -7,48 +10,42 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i, count = 0;
-char c;
-char *s;
-va_start(args, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-switch (format[++i])
-{
-case 'c':
-c = (char) va_arg(args, int);
-putchar(c);
-count++;
-break;
-case 's':
-s = va_arg(args, char *);
-while (*s)
-{
-_putchar(*s);
-s++;
-count++;
-}
-break;
-case '%':
-_putchar('%');
-count++;
-break;
-default:
-_putchar('%');
-_putchar(format[i]);
-count += 2;
-break;
-}
-}
-else
-{
-_putchar(format[i]);
-count++;
-}
-}
-va_end(args);
-return (count);
+	va_list args;
+	int i, count = 0;
+	char c, *s;
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[++i])
+			{
+				case 'c':
+					c = (char) va_arg(args, int);
+					putchar(c);
+					count++;
+					break;
+				case 's':
+					count += puts(va_arg(args, char *));
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
