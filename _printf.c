b81898477cac219be084b4a0137f02a07ba87780
dhahiba <dhhiba1991@gmail.com>
2023-04-27 06:04:16 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - ...
  * @format: ...
@@ -7,43 +8,58 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i, count = 0;
-char c;
-char *s;
-va_start(args, format);
-for (i = 0; format[i] != '\0'; i++)
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - ...
+ * @buffer: ...
+ * @buff_ind: ...
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-if (format[i] == '%')
-{
-switch (format[++i])
-{
-case 'c':
-c = (char) va_arg(args, int);
-putchar(c);
-count++;
-break;
-case 's':
-s = va_arg(args, char *);
-while (*s)
-{
-_putchar(*s);
-s++;
-count++; }
-break;
-case '%':
-_putchar('%');
-count++;
-break;
-default:
-_putchar('%');
-_putchar(format[i]);
-count += 2;
-break;
-}}
-else
-{
-_putchar(format[i]);
-count++; }
-} va_end(args);
-return (count); }
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
+}
