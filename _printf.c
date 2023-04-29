544396f26057f8dfa6cde7d88267ab91570d2062
dhahiba <dhhiba1991@gmail.com>
2023-04-29 05:19:30 -0700

    printfn

--- _printf.c
+++ _printf.c
@@ -1,106 +1,50 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
-<<<<<<< HEAD
-void print_buffer(char buffer[], int *buff_ind);
-=======
-
-
->>>>>>> 16917705d18a07fec3547aa52a0718349dcc02db
 /**
  * _printf - ...
  * @format: ...
  * Return: ...
  */
+
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+va_list args;
+int i, count = 0;
+char c;
+char *s;
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - ...
- * @buffer: ...
- * @buff_ind: ...
- */
-void print_buffer(char buffer[], int *buff_ind)
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-*buff_ind = 0;
-=======
-	va_list args;
-	int i, count = 0;
-
-	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			switch (format[++i])
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += puts(va_arg(args, char *));
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				default:
-					break;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-	}
-	va_end(args);
-	return (count);
->>>>>>> 16917705d18a07fec3547aa52a0718349dcc02db
-}
+if (format[i] == '%')
+{
+switch (format[++i])
+{
+case 'c':
+c = (char) va_arg(args, int);
+putchar(c);
+count++;
+break;
+case 's':
+s = va_arg(args, char *);
+while (*s)
+{
+putchar(*s);
+s++;
+count++; }
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+putchar(format[i]);
+count += 2;
+break; }}
+else
+{
+putchar(format[i]);
+count++; }}
+va_end(args);
+return (count); }
